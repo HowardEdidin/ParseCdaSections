976bafdeaaf08fc601fa71830c987a0d9c7a42e8
Howard Edidin <hse@msn.com>
2017-04-22 11:09:47 -0500

    initial

--- ParseCdaSections/server.js
+++ ParseCdaSections/server.js
@@ -1,8 +1,37 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+"use strict";
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var Http = require("http");
+var Port = process.env.PORT || 1337;
+
+Http.createServer(function(req, res) {
+
+    var bb = require("blue-button");
+
+    if (req.query.id && req.body.XmlDoc) {
+
+        var doc = req.body.XmlDoc;
+        var section = req.query.id;
+
+
+        var result = bb.parseString(doc, { component: section });
+
+        if (result) {
+
+            res.writeHead(200, { 'Content-Type': "application/json", 'x-ms-description': "Success" });
+            res.end(JSON.stringify(result, null, 4));
+
+        } else {
+
+            res.writeHead(400,
+                "Invalid Document Type",
+                { 'Content-Type': "application/json", 'x-ms-description': "Bad Request" });
+            res.end();
+        }
+    } else {
+        res.writeHead(405,
+            "Missing XmlDoc or Section Id",
+            { 'Content-Type': "application/json", 'x-ms-description': "Method No Allowed" });
+        res.end();
+    }
+
+}).listen(Port);
